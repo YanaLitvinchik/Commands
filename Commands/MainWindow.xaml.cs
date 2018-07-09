28a322679c0e3a1f87ac1806efe6e534277e3dc1
student <student@K09-03>
2018-07-09 17:00:00 +0300

    Add project files.

---
+++ Commands/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Commands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("NEW command works.", "Message", 
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() ;
+            if (ofd.ShowDialog() == true)
+            {
+                string path = ofd.FileName;
+                FileStream fs = new FileStream(path, FileMode.Open);
+                StreamReader sr = new StreamReader(fs,Encoding.Default);
+                string content = sr.ReadToEnd();
+                sr.Close();
+                fs.Close();
+                textbox1.Text = content;
+            }
+        }
+
+       
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenCommand_Executed_1(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                string path = ofd.FileName;
+                FileStream fs = new FileStream(path, FileMode.Open);
+                StreamReader sr = new StreamReader(fs, Encoding.Default);
+                string content = sr.ReadToEnd();
+                sr.Close();
+                fs.Close();
+                textbox1.Text = content;
+            }
+        }
+
+      
+    }
+}
